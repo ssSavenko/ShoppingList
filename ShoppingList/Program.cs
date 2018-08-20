301265549a6e601333c97b7c4e4c650616e08abd
ss.savenko <ss.savenko@gmail.com>
2018-08-20 19:01:10 +0300

    Close #3

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -12,7 +12,7 @@ namespace ShoppingList
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
